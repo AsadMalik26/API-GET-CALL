b8372735137bb30c8249d740170ab46f3e7a971b
AsadMalik26 <84629119+AsadMalik26@users.noreply.github.com>
2022-04-09 16:04:07 +0500

    CRUD - DELETE API
    Steps
    Detect onlick button -> picked up clicked button (this) -> access parentDiv using closest -> picked up id attribute -> then run ajax -> method DELETE on the url

--- jqcrud.js
+++ jqcrud.js
@@ -1,5 +1,6 @@
 $(function () {
   loadRecipie();
+  $("#recipie").on("click", ".btn-danger", handleDelete);
   //$("#addBtn").click(addRecipie);
 });
 /*
@@ -17,7 +18,20 @@ function addRecipie() {
   });
 }
 */
-
+function handleDelete() {
+  var btn = $(this);
+  var parentDiv = btn.closest(".recipie");
+  let id = parentDiv.attr("data-id");
+  console.log(id);
+  $.ajax({
+    url:"https://usman-recipes.herokuapp.com/api/recipes/"+id,
+    method: "DELETE",
+    success: function(){
+      loadRecipie();
+    }
+  });
+  // console.log("handle Delete");
+}
 function loadRecipie() {
   $.ajax({
     url: "https://usman-recipes.herokuapp.com/api/recipes",
@@ -26,10 +40,11 @@ function loadRecipie() {
       console.log(response);
       var recipie = $("#recipie");
       recipie.empty();
-      for(var i = 0; i<response.length; i++)
-      {
+      for (var i = 0; i < response.length; i++) {
         var rec = response[i];
-        recipie.append(`<div class="recipie"><h3>${rec.title}</h3><p><button class="ml-3 btn btn-danger btn-sm float-right">Delete</button>${rec.body}</p></div>`)
+        recipie.append(
+          `<div class="recipie" data-id="${rec._id}"><h3>${rec.title}</h3><p><button class="btn btn-danger btn-sm float-right">Delete</button><button class="ml-3 btn btn-warning btn-sm float-right">Edit</button>${rec.body}</p></div>`
+        );
       }
     },
   });
